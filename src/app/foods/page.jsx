ed3fee3e3d3ca5e2fdd9c8660fb6b7d038431b5f
MD ABDUR RAHMAN SHOAIB <mdabdurrahmanshoaib@gmail.com>
2026-04-10 10:23:31 +0600

    Recap Data Fetching and Display Data

---
+++ src/app/foods/page.jsx
@@ -0,0 +1,25 @@
+import FoodCard from "@/components/FoodCard";
+import React from "react";
+
+const FoodsPage = async () => {
+  // const res = await fetch('https://phi-lab-server.vercel.app/api/v1/lab/foods');
+  // const foods = await res.json();
+
+  const res = await fetch("https://phi-lab-server.vercel.app/api/v1/lab/foods");
+  const data = await res.json();
+  const foods = data.data;
+  console.log(foods);
+
+  return (
+    <div className="w-10/12 mx-auto">
+      <h2>foods: {foods.length}</h2>
+      <div className="grid md:grid-cols-3 lg:grid-cols-4 gap-4">
+        {foods.map((food) => (
+          <FoodCard key={food.id} food={food}></FoodCard>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default FoodsPage;
--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -7,6 +7,9 @@ const Posts = ({postsPromise}) => {
   return (
     <div>
       <h2 className="text-4xl">Post: {posts.length}</h2>
+     {
+      posts.map((post) =>(  <ol key={post.id} className='text-3xl font-semibold border my-5 mx-5 p-5'><li>{post.title}</li></ol>) )
+     }
     </div>
   );
 };
